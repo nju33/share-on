bc83b59939dfa7586d8fd70b514aa230fffe337a
nju33 <nju33.ki@gmail.com>
2017-02-18 04:09:24 +0900

    Implement

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,8 +1,10 @@
 'use strict'
 
+import fs from 'fs';
 import path from 'path';
 import {app, BrowserWindow, Tray, Menu, ipcMain} from 'electron';
-// import menubar from 'menubar';
+import got from 'got';
+import FormData from 'form-data';
 
 let mainWindow;
 let tray = null;
@@ -11,84 +13,94 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:${require('../../../config').port}`
   : `file://${__dirname}/index.html`
 
+function createWindow(url) {
+  const bounds = tray.getBounds();
+  trayWindow = new BrowserWindow({
+    width: 250,
+    height: 100,
+    x: bounds.x - 108,
+    y: 10,
+    frame: false,
+    movable: false,
+    resizable: false,
+    closable: false,
+    alwaysOnTop: true,
+    fullscreenable: false
+  });
+  trayWindow.loadURL(url);
+  return trayWindow;
+}
+
 function createTray() {
   if (tray !== null) {
     return tray;
   }
 
   tray = new Tray(path.join(__dirname, 'images/tray.png'));
+  tray.setToolTip('Share On');
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      click() {
+        app.quit();
+      }
+    }
+  ]))
+  tray.on('drop-files', (e, files) => {
+    if (trayWindow !== null) {
+      return;
+    }
 
-  const bounds = tray.getBounds();
-  console.log(bounds);
-  trayWindow = new BrowserWindow({
-    // width: 100,
-    // height: 50,
-    // x: bounds.x - 50,
-    // y: 10,
-    // titleBarStyle: 'hidden',
-    // resizable: false,
-    // closable: false,
-    // alwaysOnTop: true,
-    // fullscreenable: false
+    if (files.length > 1) {
+      const code = '';
+      const msg = 'Please select only one file';
+      trayWindow = createWindow(`${winURL}?error&code=${code}&msg=${msg}`);
+      return;
+    }
+
+    const form = new FormData();
+    form.append('file', fs.createReadStream(files[0]));
+    got.post('https://file.io', {
+      body: form,
+      json: true
+    })
+      .then(res => {
+        if (res.body.success) {
+          const encURI = encodeURIComponent(res.body.link);
+          trayWindow = createWindow(`${winURL}?success&uri=${encURI}`);
+        } else {
+          const code = '';
+          const msg = 'Unknown error';
+          trayWindow = createWindow(`${winURL}?error&code=${code}&msg=${msg}`);
+        }
+      })
+      .catch(({statusCode, statusMessage}) => {
+        const code = statusCode;
+        const msg = encodeURIComponent(statusMessage);
+        trayWindow = createWindow(`${winURL}?error&code=${code}&msg=${msg}`);
+      });
   });
-
-  trayWindow.loadURL(winURL);
-  // if (mb === null) {
-  //   mb = menubar({
-  //     preloadWindow: true,
-  //     icon: path.join(__dirname, 'images/tray.png')
-  //   });
-  //   mb.on('create-window', () => {
-  //     mb.window.loadURL(winURL);
-  //     mb.tray.on('drop-files', (e, files) => {
-  //       if (files.length !== 1) {
-  //       }
-  //     });
-  //     console.log(mb.tray._events);
-  //   });
-  // }
-  // return mb;
 }
-//
-// ipcMain.on('createTray:req', ({sender}) => {
-//   const _tray = createTray();
-//   _tray.on('drop', e => {
-//     sender.send('tray:log', e);
-//   });
-//
-//   sender.send('createTray:res');
-//   // process.nextTick(() => {
-//   // setTimeout(() => {
-//     // e.sender.send('createTray:res', _tray);
-//   // }, 1000)
-// });
 
-// function createWindow () {
-//   /**
-//    * Initial window options
-//    */
-//   mainWindow = new BrowserWindow({
-//     height: 600,
-//     width: 800
-//   })
-//
-//   mainWindow.loadURL(winURL)
-//
-//   mainWindow.on('closed', () => {
-//     mainWindow = null
-//   })
-//
-//   // eslint-disable-next-line no-console
-//   console.log('mainWindow opened')
-// }
-
+// let win = null;
 app.on('ready', () => {
-  // if (process.env.NODE_ENV !== 'production') {
-  //   let win = new BrowserWindow({show: false});
-  //   win.loadURL(winURL);
-  // }
-
+  // win = new BrowserWindow({
+  //   x: 0,
+  //   y: 0,
+  //   width: 250,
+  //   height: 100,
+  //   frame: false
+  // });
+  // win.loadURL(winURL);
+  app.dock.hide();
   createTray();
+});
+
+ipcMain.on('close-window', () => {
+  if (trayWindow !== null) {
+    trayWindow.destroy();
+    trayWindow = null;
+  }
 });
 
 app.on('window-all-closed', () => {
--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -1,79 +1,199 @@
 <template>
-  <div id="#app">
-    <landing-page></landing-page>
+  <div>
+    <div ref="success" class="root success" v-if="isSuccess" @click="handleSuccessClick($electron)">
+      <span v-text="uri" class="message success"></span>
+      <span class="action success">Copied!!</span>
+    </div>
+    <div class="root error" v-if="isError" @click="handleErrorClick($electron)">
+      <span v-text="errorMessage" class="message error"></span>
+      <span v-if="errorCode" v-text="errorCode" class="action error"></span>
+    </div>
+    <!-- <landing-page></landing-page> -->
   </div>
 </template>
 
 <script>
   import path from 'path';
+  import queryString from 'query-string';
   import LandingPage from 'components/LandingPageView';
-  import './imgs/tray.png';
+
+  let tid = null;
+  let tcid = null;
 
   export default {
     components: {
       LandingPage
     },
+    data() {
+      return {
+        uri: null,
+        isSuccess: false,
+        // uri: 'http://file.io/akjlD',
+        // isSuccess: true,
+        isError: false,
+        errorMessage: null,
+        errorCode: null
+        // isError: true,
+        // errorMessage: 'adlsfj',
+        // errorCode: 500,
+      }
+    },
+    methods: {
+      handleSuccessClick({ipcRenderer}) {
+        this.$electron.clipboard.writeText(this.$data.uri);
+        if (tcid !== null) {
+          return;
+        }
+        if (tid !== null) {
+          clearTimeout(tid);
+        }
+        this.$refs.success.classList.add('active');
+        tid = setTimeout(() => {
+          this.$refs.success.classList.remove('active');
+          tid = null;
+          tcid = setTimeout(() => {
+            tcid = null;
+            ipcRenderer.send('close-window');
+          }, 1000);
+        }, 2000)
+      },
+      handleErrorClick({ipcRenderer}) {
+        ipcRenderer.send('close-window');
+      }
+    },
     mounted() {
-      const {ipcRenderer} = this.$electron;
-      const {Tray} = this.$electron.remote;
-      const win = this.$electron.remote.getCurrentWindow();
-      let tray = null;
+      const parsedSearch = queryString.parse(location.search);
 
-      ipcRenderer.send('createTray:req');
-      ipcRenderer.on('createTray:res', e => {
-        // ipcRenderer.on('tray:log', (e, log) => {
-        ipcRenderer.on('tray:log', function() {
-          console.log(arguments);
-        });
-      });
-
-      // createTray();
-      //
-      // function createTray() {
-      //   if (tray === null) {
-      //     // const iconPath = path.join(__dirname, './images/tray.png');
-      //     // console.log(iconPath);
-      //     tray = new Tray('./imgs/tray.png');
-      //   }
-      //   return tray;
-      // }
-
-      // debugger
-      // ipcRenderer.send('createTray:req');
-      // // ipcRenderer.on('createTray:res', (e, tray) => {
-      // ipcRenderer.on('createTray:res', function(e, tray) {
-      //   console.log(arguments);
-      //   tray.on('drop', e => {
-      //     console.log(e);
-      //   });
-      // })
+      if ('success' in parsedSearch) {
+        this.$data.isSuccess = true;
+        this.$data.uri = decodeURIComponent(parsedSearch.uri);
+      } else if ('error' in parsedSearch) {
+        this.$data.isError = true;
+        this.$data.errorCode = parsedSearch.code;
+        this.$data.errorMessage = decodeURIComponent(parsedSearch.msg);
+      }
     }
   }
 </script>
 
 <style>
-  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+  body {
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 14px;
+  }
+</style>
 
-  * {
+<style scope>
+  body {
     margin: 0;
-    padding: 0;
+  }
+  .root {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    user-select: none;
   }
 
-  html,
-  body { height: 100%; }
+  .root.success {
+    /*background-image: linear-gradient(to bottom, #176c34, #1b813e);*/
+    background: #1b813e;
+    transition: 1.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  }
 
-  body {
-    align-items: center;
-    background:
-      radial-gradient(
-        ellipse at center,
-        rgba(255, 255, 255, 1) 0%,
-        rgba(229, 229, 229, .85) 100%
-      );
-    background-position: center;
-    display: flex;
-    font-family: Lato, Helvetica, sans-serif;
-    justify-content: center;
-    text-align: center;
+  .root.success:before,
+  .root.success:after {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .root.success:before {
+    background-image: linear-gradient(to bottom, #176c34, transparent);
+    transition: 1.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+    opacity: .75;
+  }
+
+  .root.success:after {
+    background-image: linear-gradient(to bottom, #694b15, #1b813e);
+    transition: 1s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+    opacity: 0;
+  }
+
+  .root.success.active:before {
+    opacity: 0;
+  }
+
+  .root.success.active:after {
+    opacity: .75;
+  }
+
+  .root.error {
+    background: #961f32;
+    color:
+  }
+
+  .root.error:before {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(to bottom, #811b2b, transparent);
+    transition: 1.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+    opacity: .75;
+  }
+
+  .message {
+    margin: 0 auto;
+    display: inline-block;
+    padding: .5em .75em;
+    border-radius: 5px;
+    font-size: 1.2em;
+    position: relative;
+    z-index: 2;
+    color: rgba(255,255,255,.89);
+  }
+
+  .message.success {
+    top: -.25em;
+  }
+
+  .action.success {
+    display: block;
+    position: absolute;
+    z-index: 2;
+    right: 50%;
+    transform: translate(50%, 200%);
+    bottom: 1em;
+    padding: .3em .5em;
+    font-size: .75em;
+    color: rgba(255,255,255,.89);
+    border: 1px solid rgba(255,255,255,.75);
+    background: rgba(255,255,255,.3);
+    transition: .5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  }
+
+  .active .action.success {
+    transform: translate(50%, 0);
+  }
+
+  .action.error {
+    display: block;
+    position: absolute;
+    z-index: 2;
+    top: .5em;
+    right: 50%;
+    transform: translate(50%, 0%);
+    padding: .3em .5em;
+    font-size: .75em;
+    color: rgba(255,255,255,.55);
   }
 </style>
